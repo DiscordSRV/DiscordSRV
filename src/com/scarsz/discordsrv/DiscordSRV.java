911f634054f6858dbd1a10ae918072f89852828e
Scarsz <Scarsz@users.noreply.github.com>
2016-02-09 17:39:37 -0800

    GitHub doesn't know how formatting works

--- src/com/scarsz/discordsrv/DiscordSRV.java
+++ src/com/scarsz/discordsrv/DiscordSRV.java
@@ -133,43 +133,41 @@ public class DiscordSRV extends JavaPlugin{
 	}
 	
 	public static String requestHttp(String requestUrl){
-        String sourceLine = null;
-
-        URL address = null;
-		try {
-			address = new URL(requestUrl);
-		} catch (MalformedURLException ex) {
-			ex.printStackTrace();
-		}
-
-        InputStreamReader pageInput = null;
-		try {
-			pageInput = new InputStreamReader(address.openStream());
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-        BufferedReader source = new BufferedReader(pageInput);
-
-        try {
-            sourceLine = source.readLine();
-        } catch (IOException ex) {
-        	ex.printStackTrace();
-        }
-        
+		String sourceLine = null;
+		
+        	URL address = null;
+			try {
+				address = new URL(requestUrl);
+			} catch (MalformedURLException ex) {
+				ex.printStackTrace();
+			}
+	
+	        InputStreamReader pageInput = null;
+			try {
+				pageInput = new InputStreamReader(address.openStream());
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+	        BufferedReader source = new BufferedReader(pageInput);
+	        
+	        try {
+	        	sourceLine = source.readLine();
+	        } catch (IOException ex) {
+	        	ex.printStackTrace();
+	        }
+	        
 		return sourceLine;
 	}
-    public static Boolean testChannel(TextChannel channel){
-    	return channel != null;
-    }
-	public static TextChannel getChannel(String name)
-    {
-    	if (api == null) return null;
-        for (Guild g : api.getGuilds())
-            for (TextChannel c : g.getTextChannels())
-                if (c.getName().equals(name))
-                	return c;
-        return null;
-    }
+	public static Boolean testChannel(TextChannel channel){
+		return channel != null;
+	}
+	public static TextChannel getChannel(String name){
+		if (api == null) return null;
+		for (Guild g : api.getGuilds())
+			for (TextChannel c : g.getTextChannels())
+				if (c.getName().equals(name)) return c;
+                		return null;
+        }
 	public static void sendMessage(TextChannel channel, String message){
 		if (api == null || channel == null || (!channel.checkPermission(api.getSelfInfo(), Permission.MESSAGE_READ) || !channel.checkPermission(api.getSelfInfo(), Permission.MESSAGE_WRITE))) return;
 		message = ChatColor.stripColor(message).replace("[m", "").replaceAll("\\[[0-9]{1,2};[0-9]{1,2};[0-9]{1,2}m", "").replaceAll("\\[[0-9]{1,3}m", "").replace("[m", "").replaceAll("\\[[0-9]{1,2};[0-9]{1,2};[0-9]{1,2}m", "").replaceAll("\\[[0-9]{1,3}m", "");
