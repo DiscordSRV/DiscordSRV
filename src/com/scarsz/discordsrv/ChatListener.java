db3ac38be6dba8e0aeaf6d0b381a2659737fe47b
Scarsz <Scarsz@users.noreply.github.com>
2016-02-09 19:57:16 -0800

    Update ChatListener.java

--- src/com/scarsz/discordsrv/ChatListener.java
+++ src/com/scarsz/discordsrv/ChatListener.java
@@ -15,32 +15,32 @@ import org.bukkit.plugin.Plugin;
 import com.gmail.nossr50.api.ChatAPI;
 
 public class ChatListener implements Listener {
-	JDA api;
-	Plugin plugin;
-	Boolean usingMcMMO = false;
+    JDA api;
+    Plugin plugin;
+    Boolean usingMcMMO = false;
     public ChatListener(JDA api, Plugin plugin){
         this.api = api;
         this.plugin = plugin;
         for (Plugin activePlugin : Bukkit.getPluginManager().getPlugins()) if (activePlugin.getName().toLowerCase().contains("mcmmo")) usingMcMMO = true;
     }
-	
-	@EventHandler
+    
+    @EventHandler
     public void AsyncPlayerChatEvent(AsyncPlayerChatEvent event)
     {
-		// Super long one-liner to check for vanished players
-		//for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) if (plugin.getName().contains("VanishNoPacket")) try { if (VanishNoPacket.isVanished(event.getPlayer().getName())) return; } catch (VanishNotLoadedException e) { e.printStackTrace(); }
-		
-		TextChannel channel = DiscordSRV.getChannel(plugin.getConfig().getString("DiscordChatChannelName"));
+        // Super long one-liner to check for vanished players
+        //for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) if (plugin.getName().contains("VanishNoPacket")) try { if (VanishNoPacket.isVanished(event.getPlayer().getName())) return; } catch (VanishNotLoadedException e) { e.printStackTrace(); }
+        
+        TextChannel channel = DiscordSRV.getChannel(plugin.getConfig().getString("DiscordChatChannelName"));
         /*channel.sendMessage((event.getPlayer().getDisplayName() + " [" + event.getPlayer().getName() + "]: " + event.getMessage())
-        		.replaceAll("@", "")
-        		.replaceAll("(ง[0-9])|(ง[a-z])", "")
-        		.replaceAll("\\[[0-9]{1,2};[0-9]{1,2};[0-9]{1,2}m", "")
-        		.replaceAll("\\[[0-9]{1,3}m", ""));*/
+                .replaceAll("@", "")
+                .replaceAll("(ยง[0-9])|(ยง[a-z])", "")
+                .replaceAll("\\[[0-9]{1,2};[0-9]{1,2};[0-9]{1,2}m", "")
+                .replaceAll("\\[[0-9]{1,3}m", ""));*/
         String message = "";
         message = plugin.getConfig().getString("MinecraftChatToDiscordMessageFormat")
-    			.replace("%message%", ChatColor.stripColor(event.getMessage()))
-    			.replace("%displayname%", ChatColor.stripColor(event.getPlayer().getDisplayName()))
-    			.replace("%username%", ChatColor.stripColor(event.getPlayer().getName()));
+                .replace("%message%", ChatColor.stripColor(event.getMessage()))
+                .replace("%displayname%", ChatColor.stripColor(event.getPlayer().getDisplayName()))
+                .replace("%username%", ChatColor.stripColor(event.getPlayer().getName()));
         
         // if the server has mcMMO, check if the player is using the staff/party chat
         Boolean mcMMOStaffChatEnabled = false;
@@ -49,10 +49,10 @@ public class ChatListener implements Listener {
         if (usingMcMMO && ChatAPI.isUsingPartyChat(event.getPlayer())) mcMMOPartyChatEnabled = true;
         
         if (!event.isCancelled() && !mcMMOStaffChatEnabled && !mcMMOPartyChatEnabled) {
-        	final String finalMessage = message;
-        	Executors.newSingleThreadExecutor().submit(() -> {
-        		DiscordSRV.sendMessage(channel, finalMessage);
-        	});
+            final String finalMessage = message;
+            Executors.newSingleThreadExecutor().submit(() -> {
+                DiscordSRV.sendMessage(channel, finalMessage);
+            });
         }
     }
 }
