e9fb3838d7ddaccff8afd34ae289cb4e7021e6c6
Scarsz <Scarsz@users.noreply.github.com>
2016-05-13 22:01:10 -0700

    Add missing config options

--- src/main/java/com/scarsz/discordsrv/Lag.java
+++ src/main/java/com/scarsz/discordsrv/Lag.java
@@ -5,8 +5,7 @@ public class Lag implements Runnable {
 	private static int TICK_COUNT= 0;
 	private static long[] TICKS = new long[600];
 
-	public static String getTPSString()
-	{
+	public static String getTPSString() {
 		try {
             String tps = Double.toString(getTPS());
             return tps.length() > 4 ? tps.substring(0, 4) : tps;
@@ -15,21 +14,18 @@ public class Lag implements Runnable {
         }
 	}
 	
-	private static double getTPS()
-	{
+	private static double getTPS() {
 		return getTPS(100);
 	}
 	
-	private static double getTPS(int ticks)
-	{
+	private static double getTPS(int ticks) {
 		if (TICK_COUNT < ticks) return 20.0D;
 		int target = (TICK_COUNT - 1 - ticks) % TICKS.length;
 		long elapsed = System.currentTimeMillis() - TICKS[target];
 		return ticks / (elapsed / 1000.0D);
 	}
 
-	public void run()
-	{
+	public void run() {
 		TICKS[(TICK_COUNT % TICKS.length)] = System.currentTimeMillis();
 		TICK_COUNT+= 1;
 	}
