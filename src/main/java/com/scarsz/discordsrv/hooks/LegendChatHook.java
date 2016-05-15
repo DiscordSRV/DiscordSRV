d3ea72f9d2e5d4ee6b8d7b1820270ed0e5da3364
Time6628 <pturchan@yahoo.com>
2016-05-14 22:37:43 -0400

    LegendChat support

---
+++ src/main/java/com/scarsz/discordsrv/hooks/LegendChatHook.java
@@ -0,0 +1,26 @@
+package com.scarsz.discordsrv.hooks;
+
+import br.com.devpaulo.legendchat.api.events.ChatMessageEvent;
+import com.scarsz.discordsrv.DiscordSRV;
+import net.dv8tion.jda.JDA;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class LegendChatHook implements Listener {
+    JDA api;
+    public LegendChatHook(){
+        DiscordSRV.usingLegendChat = true;
+    }
+    @EventHandler
+    public void onchat(ChatMessageEvent event) {
+        if (DiscordSRV.plugin.getConfig().getList("lc-prohibited-channels").contains(event.getChannel().getName())) return;
+        // make sure chat channel is registered
+        if (!DiscordSRV.channels.containsKey(event.getChannel().getName())) return;
+        // make sure chat channel is linked to discord channel
+        if (DiscordSRV.channels.get(event.getChannel().getName()) == null) return;
+        // make sure message isn't blank
+        if (event.getMessage().replace(" ", "").isEmpty()) return;
+
+        DiscordSRV.processChatEvent(false, event.getSender().getPlayer(), event.getMessage(), event.getChannel().getName());
+    }
+}
