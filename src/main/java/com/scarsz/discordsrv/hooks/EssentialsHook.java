6dccd41e66aed0ebf35ad5a3ebde23e4eee82e7c
Scarsz <Scarsz@users.noreply.github.com>
2016-05-15 13:19:12 -0700

    Clean up reflection of vanish API hooks

--- src/main/java/com/scarsz/discordsrv/hooks/EssentialsHook.java
+++ src/main/java/com/scarsz/discordsrv/hooks/EssentialsHook.java
@@ -1,19 +1,17 @@
 package com.scarsz.discordsrv.hooks;
 
-import java.lang.reflect.Method;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Method;
 
 public class EssentialsHook {
 
     public static boolean isVanished(String player) {
-        Plugin ess = Bukkit.getPluginManager().getPlugin("Essentials");
-        
         try {
+            Plugin ess = Bukkit.getPluginManager().getPlugin("Essentials");
         	Method getUser = ess.getClass().getDeclaredMethod("getUser", String.class);
         	Object essentialsPlayer = getUser.invoke(ess, player);
-        
         	Method isVanished = essentialsPlayer.getClass().getDeclaredMethod("isVanished");
         
         	return (boolean) isVanished.invoke(essentialsPlayer);
--- src/main/java/com/scarsz/discordsrv/hooks/SuperVanishHook.java
+++ src/main/java/com/scarsz/discordsrv/hooks/SuperVanishHook.java
@@ -7,12 +7,10 @@ public class SuperVanishHook {
     public static boolean isVanished(String player) {    	
     	try {
         	Object VanishAPI = Class.forName("de.myzelyam.api.vanish.VanishAPI");
-
-	        Method getInvisiblePlayers = VanishAPI.getClass().getDeclaredMethod("getInvisiblePlayers");
+			Method getInvisiblePlayers = VanishAPI.getClass().getDeclaredMethod("getInvisiblePlayers");
 	        Object invisiblePlayers = getInvisiblePlayers.invoke(VanishAPI);
-	
-	        Method contains = invisiblePlayers.getClass().getDeclaredMethod("contains", String.class);
-	        return (boolean) contains.invoke(invisiblePlayers, player);
+
+	        return (boolean) invisiblePlayers.getClass().getDeclaredMethod("contains", String.class).invoke(invisiblePlayers, player);
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	        return false;
