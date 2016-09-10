7dca3e7e3132b7372146231393813691808ee322
Christoph Krüger <Androkai@users.noreply.github.com>
2016-09-10 03:41:34 +0200

    fix channels.json not being put into debug correctly part 2

--- src/main/java/com/scarsz/discordsrv/util/DebugHandler.java
+++ src/main/java/com/scarsz/discordsrv/util/DebugHandler.java
@@ -97,9 +97,9 @@ public class DebugHandler {
             while (!done)
             {
                 String line = br.readLine();
-                if (line != null)
+                if (line != null) {
                     if (!line.startsWith("/")) info.add(line);
-                else
+                } else
                     done = true;
             }
         } catch (IOException e) {
