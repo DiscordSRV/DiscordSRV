8ca43dff73219c3b833781ed55db627f6e98e272
Scarsz <Scarsz@users.noreply.github.com>
2017-02-10 17:30:49 -0800

    commentate rest of DiscordUtil

--- src/main/java/github/scarsz/discordsrv/util/DiscordUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/DiscordUtil.java
@@ -22,9 +22,9 @@ import java.util.regex.Pattern;
 public class DiscordUtil {
 
     /**
-     * Get the Role's name. If the Role is null, an empty string is returned.
-     * @param role
-     * @return
+     * Get the given Role's name
+     * @param role Role to get the name of
+     * @return The name of the Role; if the Role is null, a blank string.
      */
     public static String getRoleName(Role role) {
         return role == null ? "" : role.getName();
@@ -32,22 +32,17 @@ public class DiscordUtil {
 
     /**
      * Get the top hierarchical Role of the User in the Guild
-     * @param user
-     * @param guild
-     * @return
+     * @param user User the get the top Role of
+     * @param guild Guild that the Role should be the top of
+     * @return The top hierarchical Role
      */
     public static Role getTopRole(User user, Guild guild) {
-        Role highestRole = null;
-        for (Role role : guild.getMember(user).getRoles()) {
-            if (highestRole == null) highestRole = role;
-            else if (highestRole.getPosition() < role.getPosition()) highestRole = role;
-        }
-        return highestRole;
+        return getTopRole(guild.getMember(user));
     }
     /**
      * Get the top hierarchical Role of the Member
-     * @param member
-     * @return
+     * @param member Member to get the top role of
+     * @return The top hierarchical Role
      */
     public static Role getTopRole(Member member) {
         Role highestRole = null;
@@ -60,9 +55,9 @@ public class DiscordUtil {
 
     /**
      * Convert @mentions into Discord-compatible <@012345678901234567890> mentions
-     * @param message
-     * @param guild
-     * @return
+     * @param message Message to convert
+     * @param guild Guild to find names to convert
+     * @return Contents of the given message with names converted to mentions
      */
     public static String convertMentionsFromNames(String message, Guild guild) {
         if (!message.contains("@")) return message;
@@ -77,8 +72,8 @@ public class DiscordUtil {
 
     /**
      * Trigger a mass exodus of messages in the TextChannel given the bot has the Manage Messages permission
-     * @param channel
-     * @return
+     * @param channel The channel to delete messages from
+     * @return The amount of messages that were deleted
      */
     public static int purgeChannel(TextChannel channel) {
         if (!DiscordUtil.checkPermission(channel, Permission.MESSAGE_MANAGE)) {
@@ -109,8 +104,8 @@ public class DiscordUtil {
 
     /**
      * Return the given String with Markdown escaped. Useful for sending things to Discord.
-     * @param text
-     * @return
+     * @param text String to escape markdown in
+     * @return String with markdown escaped
      */
     public static String escapeMarkdown(String text) {
         return text.replace("_", "\\_").replace("*", "\\*").replace("~", "\\~");
@@ -128,22 +123,22 @@ public class DiscordUtil {
 
     /**
      * Send the given String message to the given TextChannel
-     * @param channel
-     * @param message
+     * @param channel Channel to send the message to
+     * @param message Message to send to the channel
      */
     public static void sendMessage(TextChannel channel, String message) {
         sendMessage(channel, message, 0);
     }
     /**
-     * Send the given String message to the given TextChannel that will expire
+     * Send the given String message to the given TextChannel that will expire in x milliseconds
      * @param channel the TextChannel to send the message to
      * @param message the message to send to the TextChannel
      * @param expiration milliseconds until expiration of message. if this is 0, the message will not expire
      */
     public static void sendMessage(TextChannel channel, String message, int expiration) {
-        if (channel == null || message == null || message.equals("") || channel.getJDA() == null ||
-                !checkPermission(channel, channel.getJDA().getSelfUser(), Permission.MESSAGE_READ) ||
-                !checkPermission(channel, channel.getJDA().getSelfUser(), Permission.MESSAGE_WRITE))
+        if (channel == null || channel.getJDA() == null || message == null || message.equals("") ||
+                !checkPermission(channel, Permission.MESSAGE_READ) ||
+                !checkPermission(channel, Permission.MESSAGE_WRITE))
             return;
 
         message = DiscordUtil.stripColor(message);
@@ -165,35 +160,90 @@ public class DiscordUtil {
         if (overflow != null) sendMessage(channel, overflow, expiration);
     }
 
+    /**
+     * Check if the bot has the given permission in the given channel
+     * @param channel Channel to check for the permission in
+     * @param permission Permission to be checked for
+     * @return true if the permission is obtained, false otherwise
+     */
     public static boolean checkPermission(Channel channel, Permission permission) {
         return checkPermission(channel, channel.getJDA().getSelfUser(), permission);
     }
+    /**
+     * Check if the given user has the given permission in the given channel
+     * @param channel Channel to check for the permission in
+     * @param user User to check permissions for
+     * @param permission Permission to be checked for
+     * @return true if the permission is obtained, false otherwise
+     */
     public static boolean checkPermission(Channel channel, User user, Permission permission) {
         return channel.getGuild().getMember(user).hasPermission(channel, permission);
     }
 
-    public static Message blockMessage(TextChannel channel, String message) {
-        return blockMessage(channel, new MessageBuilder().append(message).build());
+    /**
+     * Send the given message to the given channel, blocking the thread's execution until it's successfully sent then returning it
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     * @return The sent message
+     */
+    public static Message sendMessageBlocking(TextChannel channel, String message) {
+        return sendMessageBlocking(channel, new MessageBuilder().append(message).build());
     }
-    public static Message blockMessage(TextChannel channel, Message message) {
-        final Message[] returnedMessage = {null};
-        channel.sendMessage(message).queue(message1 -> returnedMessage[0] = message1);
-        while (returnedMessage == null) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    /**
+     * Send the given message to the given channel, blocking the thread's execution until it's successfully sent then returning it
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     * @return The sent message
+     */
+    public static Message sendMessageBlocking(TextChannel channel, Message message) {
+        try {
+            return channel.sendMessage(message).block();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+            return null;
         }
-        return returnedMessage[0];
     }
+
+    /**
+     * Send the given message to the given channel
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     */
+    public static void queueMessage(TextChannel channel, String message) {
+        channel.sendMessage(message).queue();
+    }
+    /**
+     * Send the given message to the given channel
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     */
+    public static void queueMessage(TextChannel channel, Message message) {
+        channel.sendMessage(message).queue();
+    }
+    /**
+     * Send the given message to the given channel, optionally doing something with the message via the given consumer
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     * @param consumer The consumer to handle the message
+     */
     public static void queueMessage(TextChannel channel, String message, Consumer<Message> consumer) {
         channel.sendMessage(message).queue(consumer);
     }
+    /**
+     * Send the given message to the given channel, optionally doing something with the message via the given consumer
+     * @param channel The channel to send the message to
+     * @param message The message to send to the channel
+     * @param consumer The consumer to handle the message
+     */
     public static void queueMessage(TextChannel channel, Message message, Consumer<Message> consumer) {
         channel.sendMessage(message).queue(consumer);
     }
 
+    /**
+     * Set the topic message of the given channel
+     * @param channel The channel to set the topic of
+     * @param topic The new topic to be set
+     */
     public static void setTextChannelTopic(TextChannel channel, String topic) {
         channel.getManager().setTopic(topic).queue();
     }
