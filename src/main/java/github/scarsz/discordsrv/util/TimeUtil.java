327201d4bcb7a7c02e41631e0ad7dfb799612026
Scarsz <Scarsz@users.noreply.github.com>
2017-03-21 12:39:24 -0700

    #137 neater time stamps

--- src/main/java/github/scarsz/discordsrv/util/TimeUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/TimeUtil.java
@@ -1,5 +1,6 @@
 package github.scarsz.discordsrv.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -12,10 +13,11 @@ import java.util.Date;
 public class TimeUtil {
 
     private static Date date = new Date();
+    private static SimpleDateFormat format = new SimpleDateFormat("EEE, d. MMM yyyy HH:mm:ss");
 
     public static String timeStamp() {
         date.setTime(System.currentTimeMillis());
-        return date.toString();
+        return format.format(date);
     }
 
 }
