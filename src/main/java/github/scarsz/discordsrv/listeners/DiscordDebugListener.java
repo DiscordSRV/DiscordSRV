617186b48268ae8199186fa05287bec4e8a2cac9
Scarsz <truescarsz@gmail.com>
2017-06-03 11:09:07 -0700

    debug eval

--- src/main/java/github/scarsz/discordsrv/listeners/DiscordDebugListener.java
+++ src/main/java/github/scarsz/discordsrv/listeners/DiscordDebugListener.java
@@ -1,13 +1,19 @@
 package github.scarsz.discordsrv.listeners;
 
+import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.util.DebugUtil;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Made by Scarsz
@@ -30,11 +36,40 @@ public class DiscordDebugListener extends ListenerAdapter {
                 (event.isFromType(ChannelType.TEXT) && !event.getGuild().getOwner().getUser().getId().equals(event.getAuthor().getId())) // guild owner
         ) return;
 
-        // make sure the author meant to trigger this
-        if (!event.getMessage().getRawContent().equalsIgnoreCase("discorddebug") && !event.getMessage().getRawContent().equalsIgnoreCase("discordsrvdebug")) return;
+        String message = event.getMessage().getRawContent();
 
-        DiscordUtil.deleteMessage(event.getMessage());
-        DiscordUtil.privateMessage(event.getAuthor(), DebugUtil.run(event.getAuthor().toString()));
+        if (!message.startsWith("*")) return;
+        message = message.substring(1);
+
+        if (message.matches("discorddebug|discordsrvdebug")) {
+            DiscordUtil.deleteMessage(event.getMessage());
+            DiscordUtil.privateMessage(event.getAuthor(), DebugUtil.run(event.getAuthor().toString()));
+        } else if (message.startsWith("eval ")) {
+            ScriptEngine se = new ScriptEngineManager().getEngineByName("Nashorn");
+            se.put("event", event);
+            se.put("jda", event.getJDA());
+            se.put("guild", event.getGuild());
+            se.put("channel", event.getChannel());
+            se.put("server", Bukkit.getServer());
+            se.put("plugin", DiscordSRV.getPlugin());
+
+            String statement = message.substring(5);
+            long startTime = System.currentTimeMillis();
+
+            String actualStatement = statement;
+            for (Map.Entry<String, String> aliasEntry : new HashMap<String, String>() {{
+                put("bukkit", "org.bukkit.Bukkit");
+            }}.entrySet()) {
+                actualStatement = "var " + aliasEntry.getKey() + " = " + aliasEntry.getValue() + ";" + statement;
+            }
+
+            try {
+                String result = se.eval(actualStatement).toString();
+                event.getTextChannel().sendMessage("```java\n" + statement + "\n```\nEvaluated successfully in " + (System.currentTimeMillis() - startTime) + "ms:\n```java\n" + result + "\n```").queue();
+            } catch (Exception e) {
+                event.getTextChannel().sendMessage("```java\n" + statement + "\n```\nAn exception was thrown:\n```java\n"+e+"\n```").queue();
+            }
+        }
     }
 
 }
