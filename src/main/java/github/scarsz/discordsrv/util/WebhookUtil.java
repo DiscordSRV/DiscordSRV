6a318d740d9bd7f11c16fe5c18370f64a0309b83
Scarsz <truescarsz@gmail.com>
2017-06-03 11:06:35 -0700

    add permission checks for webhook usage

--- src/main/java/github/scarsz/discordsrv/util/WebhookUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/WebhookUtil.java
@@ -4,6 +4,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import github.scarsz.discordsrv.DiscordSRV;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.Webhook;
@@ -13,12 +14,19 @@ import org.bukkit.entity.Player;
 public class WebhookUtil {
 
     static {
-        // get rid of all previous webhooks created by DiscordSRV if they don't match a good channel
-        for (Guild guild : DiscordSRV.getPlugin().getJda().getGuilds()) {
-            for (Webhook webhook : guild.getWebhooks().complete()) {
-                if (webhook.getName().startsWith("DiscordSRV") && DiscordSRV.getPlugin().getDestinationGameChannelNameForTextChannel(webhook.getChannel()) == null) {
-                    webhook.delete().reason("Purge").queue();
+        try {
+            // get rid of all previous webhooks created by DiscordSRV if they don't match a good channel
+            for (Guild guild : DiscordSRV.getPlugin().getJda().getGuilds()) {
+                for (Webhook webhook : guild.getWebhooks().complete()) {
+                    if (webhook.getName().startsWith("DiscordSRV") && DiscordSRV.getPlugin().getDestinationGameChannelNameForTextChannel(webhook.getChannel()) == null) {
+                        webhook.delete().reason("Purge").queue();
+                    }
                 }
+            }
+        } catch (Exception e) {
+            DiscordSRV.warning("Failed to purge already existing webhooks: " + e.getMessage());
+            if (DiscordSRV.getPlugin().getConfig().getInt("DebugLevel") > 0) {
+                e.printStackTrace();
             }
         }
     }
@@ -34,7 +42,14 @@ public class WebhookUtil {
         }
 
         if (targetWebhook == null) {
-            // no suitable webhook existed, create it
+            // no suitable webhook existed
+
+            if (!channel.getGuild().getMember(channel.getJDA().getSelfUser()).hasPermission(Permission.MANAGE_WEBHOOKS)) {
+                DiscordSRV.error("Can't create a webhook to deliver chat message, bot is missing permission \"Manage Webhooks\"");
+                return;
+            }
+
+            // create a webhook to use
             targetWebhook = channel.getGuild().getController().createWebhook(channel, "DiscordSRV #" + channel.getName()).complete();
             DiscordSRV.debug("Created webhook " + targetWebhook + " to deliver messages to text channel #" + channel.getName());
         }
