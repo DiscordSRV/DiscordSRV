1e113525dce69aa5128cd18c21323e3a9e7e6324
Scarsz <truescarsz@gmail.com>
2017-12-08 19:29:17 -0800

    add failed message to debug statement for DiscordChatChannelPrefix

--- src/main/java/github/scarsz/discordsrv/DiscordSRV.java
+++ src/main/java/github/scarsz/discordsrv/DiscordSRV.java
@@ -634,7 +634,7 @@ public class DiscordSRV extends JavaPlugin implements Listener {
 
         // return if doesn't match prefix filter
         if (!message.startsWith(getConfig().getString("DiscordChatChannelPrefix"))) {
-            debug("User " + player.getName() + " sent a message but it was not delivered to Discord because the message didn't start with \"" + getConfig().getString("DiscordChatChannelPrefix") + "\" (DiscordChatChannelPrefix)");
+            debug("User " + player.getName() + " sent a message but it was not delivered to Discord because the message didn't start with \"" + getConfig().getString("DiscordChatChannelPrefix") + "\" (DiscordChatChannelPrefix): \"" + message + "\"");
             return;
         }
 
