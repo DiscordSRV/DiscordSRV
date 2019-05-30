98a36e050430a88ef80a3413748a346987741659
Vankka <vankka.main@gmail.com>
2019-05-30 13:04:38 +0300

    hide jdbc url in configutil migration

--- src/main/java/github/scarsz/discordsrv/util/ConfigUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/ConfigUtil.java
@@ -133,7 +133,8 @@ public class ConfigUtil {
 
             for (String key : oldConfigMap.keySet()) {
                 if (newConfigMap.containsKey(key) && !key.startsWith("ConfigVersion")) {
-                    DiscordSRV.debug("Migrating config option " + key + " with value " + (key.toLowerCase().equals("bottoken") ? "OMITTED" : oldConfigMap.get(key)) + " to new config");
+                    DiscordSRV.debug("Migrating config option " + key + " with value " + (key.toLowerCase().equals("bottoken")
+                            || key.toLowerCase().equals("experiment_jdbcaccountlinkbackend") ? "OMITTED" : oldConfigMap.get(key)) + " to new config");
                     newConfigMap.put(key, oldConfigMap.get(key));
                 }
             }
