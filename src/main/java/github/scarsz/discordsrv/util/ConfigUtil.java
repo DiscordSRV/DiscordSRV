367aa18f3443fcf217be8b54f549849f356e1f48
Scarsz <truescarsz@gmail.com>
2019-07-10 20:48:36 -0700

    fix another issue with config migration messages and jdbc credentials

--- src/main/java/github/scarsz/discordsrv/util/ConfigUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/ConfigUtil.java
@@ -133,10 +133,7 @@ public class ConfigUtil {
 
             for (String key : oldConfigMap.keySet()) {
                 if (newConfigMap.containsKey(key) && !key.startsWith("ConfigVersion")) {
-                    DebugUtil.SENSITIVE_OPTIONS.contains(key.toLowerCase());
-
-                    DiscordSRV.debug("Migrating config option " + key + " with value " + (key.toLowerCase().equals("bottoken")
-                            || key.toLowerCase().equals("experiment_jdbcaccountlinkbackend") ? "OMITTED" : oldConfigMap.get(key)) + " to new config");
+                    DiscordSRV.debug("Migrating config option " + key + " with value " + (DebugUtil.SENSITIVE_OPTIONS.stream().anyMatch(key::equalsIgnoreCase) ? "OMITTED" : oldConfigMap.get(key)) + " to new config");
                     newConfigMap.put(key, oldConfigMap.get(key));
                 }
             }
