3a3b334bd39ffef68d12e799bba7308749a0e279
Scarsz <truescarsz@gmail.com>
2020-02-17 12:36:28 -0800

    #637

--- src/main/java/github/scarsz/discordsrv/listeners/DiscordAccountLinkListener.java
+++ src/main/java/github/scarsz/discordsrv/listeners/DiscordAccountLinkListener.java
@@ -20,8 +20,13 @@ package github.scarsz.discordsrv.listeners;
 
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.api.events.DiscordPrivateMessageReceivedEvent;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.UUID;
 
 public class DiscordAccountLinkListener extends ListenerAdapter {
 
@@ -34,6 +39,16 @@ public class DiscordAccountLinkListener extends ListenerAdapter {
 
         String reply = DiscordSRV.getPlugin().getAccountLinkManager().process(event.getMessage().getContentRaw(), event.getAuthor().getId());
         if (reply != null) event.getChannel().sendMessage(reply).queue();
+    }
+
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        // add linked role back to people when they rejoin the server
+        UUID uuid = DiscordSRV.getPlugin().getAccountLinkManager().getUuid(event.getUser().getId());
+        if (uuid != null) {
+            Role roleToAdd = DiscordUtil.getRole(event.getMember().getGuild(), DiscordSRV.config().getString("MinecraftDiscordAccountLinkedRoleNameToAddUserTo"));
+            if (roleToAdd != null) DiscordUtil.addRolesToMember(event.getMember(), roleToAdd);
+            else DiscordSRV.debug("Couldn't add user to null role");
+        }
     }
 
 }
