b0924ea9fe217476142930719ee5d839fc9ac9ac
Vankka <20226336+Vankka@users.noreply.github.com>
2020-03-14 15:48:50 +0200

    Make primary group check null safe

--- src/main/java/github/scarsz/discordsrv/objects/managers/GroupSynchronizationManager.java
+++ src/main/java/github/scarsz/discordsrv/objects/managers/GroupSynchronizationManager.java
@@ -112,7 +112,7 @@ public class GroupSynchronizationManager extends ListenerAdapter implements List
             }
 
             boolean hasGroup = DiscordSRV.config().getBoolean("GroupRoleSynchronizationPrimaryGroupOnly")
-                    ? getPermissions().getPrimaryGroup(null, player).equalsIgnoreCase(groupName)
+                    ? groupName.equalsIgnoreCase(getPermissions().getPrimaryGroup(null, player))
                     : getPermissions().playerInGroup(null, player, groupName);
             if (getPermissions().playerHas(null, player, "discordsrv.sync." + groupName)) hasGroup = true;
             if (getPermissions().playerHas(null, player, "discordsrv.sync.deny." + groupName)) hasGroup = false;
