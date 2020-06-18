2b5a36e506a7ebf164c9beb05644ab0c1eae2c78
Scarsz <truescarsz@gmail.com>
2020-06-18 12:39:34 -0700

    refactor AlertListener to use DiscordSRV#getMessageFromConfiguration

--- src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
+++ src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
@@ -7,20 +7,15 @@ import github.scarsz.discordsrv.objects.Lag;
 import github.scarsz.discordsrv.objects.MessageFormat;
 import github.scarsz.discordsrv.util.*;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
-import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.plugin.RegisteredListener;
 
-import java.awt.*;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -84,7 +79,9 @@ public class AlertListener implements Listener {
             return;
         }
 
-        DiscordSRV.config().dget("Alerts").children().forEach(alert -> {
+        List<Dynamic> alerts = DiscordSRV.config().dget("Alerts").children().collect(Collectors.toList());
+        for (int i = 0; i < alerts.size(); i++) {
+            Dynamic alert = alerts.get(i);
             String triggerEvent = alert.get("Trigger").convert().intoString();
 
             // make sure the called event matches what this alert is supposed to trigger on
@@ -131,102 +128,7 @@ public class AlertListener implements Listener {
                     });
             if (anyConditionFailed) return;
 
-            // construct the alert message
-            MessageFormat messageFormat = new MessageFormat();
-
-            Dynamic contentDynamic = alert.dget("Content");
-            if (contentDynamic.isPresent() && StringUtils.isNotBlank(contentDynamic.asString())) messageFormat.setContent(contentDynamic.asString());
-
-            if (alert.dget("Webhook").isPresent()) {
-                boolean enabled = alert.dget("Webhook.Enable").isPresent() ? alert.dget("Webhook.Enable").as(boolean.class) : true;
-                if (enabled) {
-                    messageFormat.setUseWebhooks(true);
-
-                    Dynamic avatarUrlDynamic = alert.dget("Webhook.AvatarUrl");
-                    if (avatarUrlDynamic.isPresent() && StringUtils.isNotBlank(avatarUrlDynamic.asString())) messageFormat.setWebhookAvatarUrl(avatarUrlDynamic.asString());
-
-                    Dynamic nameDynamic = alert.dget("Webhook.Name");
-                    if (nameDynamic.isPresent() && StringUtils.isNotBlank(nameDynamic.asString())) messageFormat.setWebhookName(nameDynamic.asString());
-                }
-            }
-
-            if (alert.get("Embed").isPresent()) {
-                boolean enabled = alert.dget("Embed.Enabled").isPresent() ? alert.dget("Embed.Enabled").as(Boolean.class) : true;
-                if (enabled) {
-                    Dynamic colorDynamic = alert.dget("Embed.Color");
-                    if (colorDynamic.isPresent()) {
-                        String hex = colorDynamic.asString().trim().replace("#", "");
-                        if (hex.length() == 6) {
-                            messageFormat.setColor(
-                                    new Color(
-                                            Integer.valueOf(hex.substring(0, 2), 16),
-                                            Integer.valueOf(hex.substring(2, 4), 16),
-                                            Integer.valueOf(hex.substring(4, 6), 16)
-                                    )
-                            );
-                        }
-                    }
-
-                    Dynamic authorDynamic = alert.dget("Embed.Author");
-                    if (authorDynamic.isPresent()) {
-                        Dynamic authorNameDynamic = authorDynamic.get("Name");
-                        if (authorNameDynamic.isPresent() && StringUtils.isNotBlank(authorNameDynamic.convert().intoString())) messageFormat.setAuthorName(authorNameDynamic.convert().intoString());
-                        Dynamic authorUrlDynamic = authorDynamic.get("Url");
-                        if (authorUrlDynamic.isPresent() && StringUtils.isNotBlank(authorUrlDynamic.convert().intoString())) messageFormat.setAuthorUrl(authorUrlDynamic.convert().intoString());
-                        Dynamic authorImageUrl = authorDynamic.get("ImageUrl");
-                        if (authorImageUrl.isPresent() && StringUtils.isNotBlank(authorImageUrl.convert().intoString())) messageFormat.setAuthorImageUrl(authorImageUrl.convert().intoString());
-                    }
-
-                    Dynamic thumbnailUrlDynamic = alert.dget("Embed.ThumbnailUrl");
-                    if (thumbnailUrlDynamic.isPresent() && StringUtils.isNotBlank(thumbnailUrlDynamic.convert().intoString())) messageFormat.setThumbnailUrl(thumbnailUrlDynamic.convert().intoString());
-
-                    Dynamic titleDynamic = alert.dget("Embed.Title");
-                    if (titleDynamic.isPresent()) {
-                        Dynamic textDynamic = titleDynamic.get("Text");
-                        if (textDynamic.isPresent() && StringUtils.isNotBlank(textDynamic.convert().intoString())) messageFormat.setTitle(textDynamic.convert().intoString());
-                        Dynamic urlDynamic = titleDynamic.get("Url");
-                        if (urlDynamic.isPresent() && StringUtils.isNotBlank(urlDynamic.convert().intoString())) messageFormat.setTitleUrl(urlDynamic.convert().intoString());
-                    }
-
-                    Dynamic descriptionDynamic = alert.dget("Embed.Description");
-                    if (descriptionDynamic.isPresent() && StringUtils.isNotBlank(descriptionDynamic.convert().intoString())) messageFormat.setDescription(descriptionDynamic.convert().intoString());
-
-                    Dynamic fieldsDynamic = alert.dget("Embed.Fields");
-                    if (fieldsDynamic.isPresent()) {
-                        List<MessageEmbed.Field> fields = new LinkedList<>();
-                        fieldsDynamic.children().map(Object::toString).forEach(field -> {
-                            if (field.contains(";")) {
-                                String[] parts = field.split(";");
-                                if (parts.length < 2) {
-                                    return;
-                                }
-
-                                boolean inline = parts.length < 3 || Boolean.parseBoolean(parts[2]);
-                                fields.add(new MessageEmbed.Field(parts[0], parts[1], inline, true));
-                            } else {
-                                boolean inline = Boolean.parseBoolean(field);
-                                fields.add(new MessageEmbed.Field("\u200e", "\u200e", inline, true));
-                            }
-                        });
-                        messageFormat.setFields(fields);
-                    }
-
-                    Dynamic imageUrlDynamic = alert.dget("Embed.ImageUrl");
-                    if (imageUrlDynamic.isPresent() && StringUtils.isNotBlank(imageUrlDynamic.convert().intoString())) messageFormat.setImageUrl(imageUrlDynamic.convert().intoString());
-
-                    Dynamic footerDynamic = alert.dget("Embed.Footer");
-                    if (footerDynamic.isPresent()) {
-                        Dynamic textDynamic = footerDynamic.get("Text");
-                        if (textDynamic.isPresent() && StringUtils.isNotBlank(textDynamic.convert().intoString())) messageFormat.setFooterText(textDynamic.convert().intoString());
-                        Dynamic iconUrlDynamic = footerDynamic.get("IconUrl");
-                        if (iconUrlDynamic.isPresent() && StringUtils.isNotBlank(iconUrlDynamic.convert().intoString())) messageFormat.setFooterIconUrl(iconUrlDynamic.convert().intoString());
-                    }
-
-                    Dynamic timestampDynamic = alert.dget("Embed.Timestamp");
-                    if (timestampDynamic.isPresent() && timestampDynamic.as(boolean.class)) messageFormat.setTimestamp(new Date().toInstant());
-                }
-            }
-
+            MessageFormat messageFormat = DiscordSRV.getPlugin().getMessageFromConfiguration("Alerts." + i);
             Message message = DiscordSRV.getPlugin().translateMessage(messageFormat, (content, needsEscape) -> {
                 if (content == null) return null;
 
@@ -271,7 +173,7 @@ public class AlertListener implements Listener {
             });
 
             channel.sendMessage(message).queue();
-        });
+        }
     }
 
     private void onCommand(PlayerCommandPreprocessEvent event) {
