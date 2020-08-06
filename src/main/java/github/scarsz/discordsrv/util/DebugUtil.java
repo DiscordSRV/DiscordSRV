d3d9b16cc36f8a1b781e7d251e83e53010dccd20
Vankka <20226336+Vankka@users.noreply.github.com>
2020-08-06 13:55:31 +0300

    Only put debug-info.txt to the bottom if there were no issues

--- src/main/java/github/scarsz/discordsrv/util/DebugUtil.java
+++ src/main/java/github/scarsz/discordsrv/util/DebugUtil.java
@@ -74,6 +74,11 @@ public class DebugUtil {
     public static String run(String requester, int aesBits) {
         List<Map<String, String>> files = new LinkedList<>();
         try {
+            String debugInformation = getDebugInformation();
+            boolean noIssues = debugInformation.contains("No issues detected automatically");
+            if (!noIssues) {
+                files.add(fileMap("debug-info.txt", "Potential issues in the installation", debugInformation));
+            }
             files.add(fileMap("discordsrv-info.txt", "general information about the plugin", String.join("\n", new String[]{
                     "plugin version: " + DiscordSRV.getPlugin(),
                     "config version: " + DiscordSRV.config().getString("ConfigVersion"),
@@ -127,7 +132,9 @@ public class DebugUtil {
                     PrettyUtil.beautify(getServerThread().getStackTrace())
             })));
             files.add(fileMap("system-info.txt", null, getSystemInfo()));
-            files.add(fileMap("debug-info.txt", "Potential issues in the installation", getDebugInformation()));
+            if (noIssues) {
+                files.add(fileMap("debug-info.txt", "Potential issues in the installation", debugInformation));
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return "Failed to collect debug information: " + e.getMessage() + ". Check the console for further details.";
