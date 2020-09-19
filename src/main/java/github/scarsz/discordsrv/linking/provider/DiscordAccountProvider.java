21a8661bc99d9ba68d6ab5d26b266a4428696c59
Scarsz <truescarsz@gmail.com>
2020-09-19 14:58:34 -0700

    add async methods to account providers

--- src/main/java/github/scarsz/discordsrv/linking/provider/DiscordAccountProvider.java
+++ src/main/java/github/scarsz/discordsrv/linking/provider/DiscordAccountProvider.java
@@ -1,9 +1,12 @@
 package github.scarsz.discordsrv.linking.provider;
 
+import github.scarsz.discordsrv.DiscordSRV;
 import lombok.NonNull;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
 /**
  * Represents a provider of linked Discord accounts, given a Minecraft account.
@@ -25,11 +28,33 @@ public interface DiscordAccountProvider extends AccountProvider {
      */
     String getDiscordId(@NonNull UUID player);
 
+    default void getDiscordId(@NonNull OfflinePlayer player, Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getDiscordId(player)));
+    }
+    default void getDiscordId(@NonNull UUID uuid, Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getDiscordId(uuid)));
+    }
+
+    default boolean isLinked(@NonNull OfflinePlayer player) {
+        return getDiscordId(player) != null;
+    }
     default boolean isLinked(@NonNull UUID uuid) {
         return getDiscordId(uuid) != null;
     }
-    default boolean isLinked(@NonNull OfflinePlayer player) {
-        return getDiscordId(player) != null;
+
+    default void ifLinked(@NonNull OfflinePlayer player, @NonNull Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> {
+            if (isLinked(player)) {
+                runnable.run();
+            }
+        });
+    }
+    default void ifLinked(@NonNull UUID uuid, @NonNull Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> {
+            if (isLinked(uuid)) {
+                runnable.run();
+            }
+        });
     }
 
 }
--- src/main/java/github/scarsz/discordsrv/linking/provider/MinecraftAccountProvider.java
+++ src/main/java/github/scarsz/discordsrv/linking/provider/MinecraftAccountProvider.java
@@ -1,5 +1,6 @@
 package github.scarsz.discordsrv.linking.provider;
 
+import github.scarsz.discordsrv.DiscordSRV;
 import lombok.NonNull;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
@@ -7,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
 /**
  * Represents a provider of linked Minecraft accounts, given a Discord account.
@@ -37,6 +39,17 @@ public interface MinecraftAccountProvider extends AccountProvider {
      * @return the linked {@link UUID}, null if not linked
      */
     UUID getUuid(@NonNull String userId);
+
+    default void getUuid(@NonNull Member member, Consumer<UUID> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getUuid(member)));
+    }
+    default void getUuid(@NonNull User user, Consumer<UUID> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getUuid(user)));
+    }
+    default void getUuid(@NonNull String userId, Consumer<UUID> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getUuid(userId)));
+    }
+
     /**
      * Get the {@link OfflinePlayer} associated with the given Discord member
      * @param member the member to look up
@@ -62,6 +75,16 @@ public interface MinecraftAccountProvider extends AccountProvider {
         return Bukkit.getOfflinePlayer(getUuid(userId));
     }
 
+    default void getOfflinePlayer(@NonNull Member member, Consumer<OfflinePlayer> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getOfflinePlayer(member)));
+    }
+    default void getOfflinePlayer(@NonNull User user, Consumer<OfflinePlayer> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getOfflinePlayer(user)));
+    }
+    default void getOfflinePlayer(@NonNull String userId, Consumer<OfflinePlayer> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> consumer.accept(getOfflinePlayer(userId)));
+    }
+
     default boolean isLinked(@NonNull Member member) {
         return getUuid(member.getUser()) != null;
     }
@@ -70,6 +93,28 @@ public interface MinecraftAccountProvider extends AccountProvider {
     }
     default boolean isLinked(@NonNull String userId) {
         return getUuid(userId) != null;
+    }
+
+    default void ifLinked(@NonNull Member member, @NonNull Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> {
+            if (isLinked(member)) {
+                runnable.run();
+            }
+        });
+    }
+    default void ifLinked(@NonNull User user, @NonNull Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> {
+            if (isLinked(user)) {
+                runnable.run();
+            }
+        });
+    }
+    default void ifLinked(@NonNull String userId, @NonNull Runnable runnable) {
+        Bukkit.getScheduler().runTaskAsynchronously(DiscordSRV.getPlugin(), () -> {
+            if (isLinked(userId)) {
+                runnable.run();
+            }
+        });
     }
 
 }
