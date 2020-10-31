bba1e2e0a30cf69f8d973a07c80bf1319eb7ca8f
Vankka <20226336+Vankka@users.noreply.github.com>
2020-10-31 23:47:41 +0200

    Reload the regexes

--- src/main/java/github/scarsz/discordsrv/DiscordSRV.java
+++ src/main/java/github/scarsz/discordsrv/DiscordSRV.java
@@ -894,6 +894,7 @@ public class DiscordSRV extends JavaPlugin {
         }
 
         reloadChannels();
+        reloadRegexes();
 
         // warn if the console channel is connected to a chat channel
         if (getMainTextChannel() != null && getConsoleChannel() != null && getMainTextChannel().getId().equals(getConsoleChannel().getId())) DiscordSRV.warning(LangUtil.InternalMessage.CONSOLE_CHANNEL_ASSIGNED_TO_LINKED_CHANNEL);
