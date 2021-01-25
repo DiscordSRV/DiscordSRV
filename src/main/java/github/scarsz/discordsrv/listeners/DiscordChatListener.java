eae12464da65fd470664e98898eff7d29d43cd11
Vankka <20226336+Vankka@users.noreply.github.com>
2021-01-25 18:24:13 +0200

    Fix %toprolecolor% for attachments
    Fixes https://github.com/DiscordSRV/DiscordSRV/issues/993

--- src/main/java/github/scarsz/discordsrv/listeners/DiscordChatListener.java
+++ src/main/java/github/scarsz/discordsrv/listeners/DiscordChatListener.java
@@ -120,6 +120,7 @@ public class DiscordChatListener extends ListenerAdapter {
         }
 
         List<Role> selectedRoles = DiscordSRV.getPlugin().getSelectedRoles(event.getMember());
+        Role topRole = !selectedRoles.isEmpty() ? selectedRoles.get(0) : null;
 
         // if there are attachments send them all as one message
         if (!event.getMessage().getAttachments().isEmpty()) {
@@ -134,6 +135,8 @@ public class DiscordChatListener extends ListenerAdapter {
                         replacePlaceholders(placedMessage, event, selectedRoles, attachment.getUrl()));
                 if (DiscordSRV.config().getBoolean("Experiment_MCDiscordReserializer_ToMinecraft")) placedMessage = DiscordUtil.convertMentionsToNames(placedMessage);
                 Component component = MessageUtil.toComponent(placedMessage);
+                component = replaceTopRoleColor(component, topRole != null ? topRole.getColorRaw() : DiscordUtil.DISCORD_DEFAULT_COLOR.getRGB());
+
                 DiscordGuildMessagePostProcessEvent postEvent = DiscordSRV.api.callEvent(new DiscordGuildMessagePostProcessEvent(event, preEvent.isCancelled(), component));
                 if (postEvent.isCancelled()) {
                     DiscordSRV.debug("DiscordGuildMessagePostProcessEvent was cancelled, attachment send aborted");
@@ -203,9 +206,7 @@ public class DiscordChatListener extends ListenerAdapter {
         if (authorLinkedUuid != null) authorPlayer = Bukkit.getPlayer(authorLinkedUuid);
 
         formatMessage = PlaceholderUtil.replacePlaceholders(formatMessage, authorPlayer);
-
         Component component = MessageUtil.toComponent(formatMessage);
-        Role topRole = !selectedRoles.isEmpty() ? selectedRoles.get(0) : null;
         component = replaceTopRoleColor(component, topRole != null ? topRole.getColorRaw() : DiscordUtil.DISCORD_DEFAULT_COLOR.getRGB());
 
         DiscordGuildMessagePostProcessEvent postEvent = DiscordSRV.api.callEvent(new DiscordGuildMessagePostProcessEvent(event, preEvent.isCancelled(), component));
