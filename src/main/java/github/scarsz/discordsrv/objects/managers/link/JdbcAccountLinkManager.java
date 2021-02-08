bd67bc94a106da414a3185da2273feba7d146550
Scarsz <truescarsz@gmail.com>
2021-02-08 15:14:20 -0800

    #1015

--- src/main/java/github/scarsz/discordsrv/objects/managers/link/JdbcAccountLinkManager.java
+++ src/main/java/github/scarsz/discordsrv/objects/managers/link/JdbcAccountLinkManager.java
@@ -87,7 +87,7 @@ public class JdbcAccountLinkManager extends AbstractAccountLinkManager {
                 return false;
             }
 
-            if (!matcher.group("protocol").equalsIgnoreCase("jdbc")) {
+            if (!matcher.group("proto").equalsIgnoreCase("jdbc")) {
                 if (!quiet) DiscordSRV.error("Not using JDBC because the protocol of the JDBC URL is wrong!");
                 return false;
             }
