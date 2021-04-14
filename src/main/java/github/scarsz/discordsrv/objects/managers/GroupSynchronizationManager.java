ff574c0e4245b3a017df2bed9e44252603cffd98
Vankka <20226336+Vankka@users.noreply.github.com>
2021-04-14 15:43:34 +0300

    Don't check permissions from within getPermissions in GroupSynchronizationManager

--- src/main/java/github/scarsz/discordsrv/objects/managers/GroupSynchronizationManager.java
+++ src/main/java/github/scarsz/discordsrv/objects/managers/GroupSynchronizationManager.java
@@ -657,7 +657,7 @@ public class GroupSynchronizationManager extends ListenerAdapter implements List
                 }
                 return permission = provider.getProvider();
             } catch (ClassNotFoundException e) {
-                if (!warnedAboutMissingVault && DiscordSRV.getPlugin().isGroupRoleSynchronizationEnabled()) {
+                if (!warnedAboutMissingVault && DiscordSRV.getPlugin().isGroupRoleSynchronizationEnabled(false)) {
                     DiscordSRV.error("Group synchronization failed: Vault classes couldn't be found (did it enable properly?). Vault is required for synchronization to work.");
                     warnedAboutMissingVault = true;
                 }
