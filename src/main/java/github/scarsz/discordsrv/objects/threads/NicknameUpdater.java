e5025f56539fb169df7e9ee06c642dcb3e56cc34
Dinty1 <dintybmrt@gmail.com>
2021-04-24 23:15:48 +0100

    Limit nicknames to 32 characters

--- src/main/java/github/scarsz/discordsrv/objects/threads/NicknameUpdater.java
+++ src/main/java/github/scarsz/discordsrv/objects/threads/NicknameUpdater.java
@@ -130,6 +130,10 @@ public class NicknameUpdater extends Thread {
         }
 
         nickname = MessageUtil.strip(nickname);
+        if (nickname.length() > 32) {
+            DiscordSRV.debug("The new nickname for " + offlinePlayer.getName() + " is too long, reducing it to 32 characters.");
+            nickname = nickname.substring(0, 32);
+        }
         DiscordUtil.setNickname(member, nickname);
     }
 }
