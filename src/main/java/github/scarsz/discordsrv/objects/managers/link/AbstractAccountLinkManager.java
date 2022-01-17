961ca8ec9d142d28c5b0db52546e2c4e0a67ccda
Dylan Hackworth <dylhack@gmail.com>
2022-01-16 21:55:30 -0600

    refactor: simplified the approach

--- src/main/java/github/scarsz/discordsrv/objects/managers/link/AbstractAccountLinkManager.java
+++ src/main/java/github/scarsz/discordsrv/objects/managers/link/AbstractAccountLinkManager.java
@@ -49,6 +49,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class AbstractAccountLinkManager extends AccountLinkManager {
+    private static final int[] CHAR_CODES = new int[] {
+            // 0 - 9
+            48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+            // A - Z
+            65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
+            78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+            // a - z
+            97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+            111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
+    };
 
     @Getter
     protected final Map<String, UUID> linkingCodes = new ConcurrentHashMap<>();
@@ -60,21 +70,8 @@ public abstract class AbstractAccountLinkManager extends AccountLinkManager {
         do {
             codeBuilder = new StringBuilder();
             for (int i = 0; i < 4; i += 1) {
-                int j = ThreadLocalRandom.current().nextInt(0, 3);
-                int k;
-                switch (j) {
-                    case 0:
-                        // A - Z
-                        k = ThreadLocalRandom.current().nextInt(65, 91);
-                        break;
-                    case 1:
-                        // a - z
-                        k = ThreadLocalRandom.current().nextInt(97, 123);
-                        break;
-                    default:
-                        // 0 - 9
-                        k = ThreadLocalRandom.current().nextInt(48, 59);
-                }
+                int j = ThreadLocalRandom.current().nextInt(63);
+                int k = CHAR_CODES[j];
                 codeBuilder.append(Character.toChars(k));
             }
             codeString = codeBuilder.toString();
