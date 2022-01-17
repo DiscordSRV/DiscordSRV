5ddc3e1d48069b2fee35b2d9b101036adc2d12c1
Dylan Hackworth <dylhack@gmail.com>
2022-01-16 20:01:06 -0600

    fix: incorrect origin for A-Z  and bound for 0-9

--- src/main/java/github/scarsz/discordsrv/objects/managers/link/AbstractAccountLinkManager.java
+++ src/main/java/github/scarsz/discordsrv/objects/managers/link/AbstractAccountLinkManager.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -65,7 +65,7 @@ public abstract class AbstractAccountLinkManager extends AccountLinkManager {
                 switch (j) {
                     case 0:
                         // A - Z
-                        k = ThreadLocalRandom.current().nextInt(64, 91);
+                        k = ThreadLocalRandom.current().nextInt(65, 91);
                         break;
                     case 1:
                         // a - z
@@ -73,7 +73,7 @@ public abstract class AbstractAccountLinkManager extends AccountLinkManager {
                         break;
                     default:
                         // 0 - 9
-                        k = ThreadLocalRandom.current().nextInt(48, 58);
+                        k = ThreadLocalRandom.current().nextInt(48, 59);
                 }
                 codeBuilder.append(Character.toChars(k));
             }
