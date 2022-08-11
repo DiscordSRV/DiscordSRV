088fbf09d14f573648aed342b2ee1ee52face217
Austin Shapiro <truescarsz@gmail.com>
2022-08-11 08:16:13 -0700

    fully document SlashCommandProvider

--- src/main/java/github/scarsz/discordsrv/api/commands/SlashCommandProvider.java
+++ src/main/java/github/scarsz/discordsrv/api/commands/SlashCommandProvider.java
@@ -1,9 +1,21 @@
 package github.scarsz.discordsrv.api.commands;
 
+import github.scarsz.discordsrv.api.ApiManager;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
+import org.bukkit.plugin.Plugin;
+
 import java.util.Set;
 
 /**
- * Interface to be added to {@link org.bukkit.plugin.java.JavaPlugin}'s to provide Discord slash command data
+ * Provides {@link PluginCommandData} datum to DiscordSRV's {@link ApiManager}.
+ * {@link Plugin}s implementing this interface are automatically registered.
+ * To manually register a provider, use {@link ApiManager#addSlashCommandProvider(SlashCommandProvider)}.
+ * To handle commands, create a method in a {@link SlashCommandProvider} with the {@link SlashCommand} annotation.
+ * @see SlashCommand
+ * @see ApiManager#addSlashCommandProvider(SlashCommandProvider)
+ * @see CommandInteraction
+ * @see InteractionHook
  */
 public interface SlashCommandProvider {
 
