eb1235ee47204229e1bdbe4dc7f6c0be63ef19f1
Henri Schubin <vankka.main@gmail.com>
2022-12-06 15:58:21 +0200

    Reorganize dependencies, fix dependency configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,16 +75,20 @@ repositories {
 }
 
 dependencies {
+    // Paper API
     compileOnly("com.destroystokyo.paper:paper-api:${minecraftVersion}-R0.1-SNAPSHOT") {
-        exclude("commons-lang") // we have our own
+        exclude("commons-lang") // Exclude lang in favor of our own lang3
     }
+    
+    // JDA
     api("net.dv8tion:JDA:4.4.0_352.fix-2") {
         exclude("opus-java") // we don't use voice features
     }
+    
+    // Config
     api("github.scarsz:configuralize:1.3.2")
-    implementation("org.bstats:bstats-bukkit:2.2.1")
-
-    // logging
+    
+    // Logging
     implementation("me.scarsz:jdaappender:1.0.3")
     implementation("org.slf4j:slf4j-jdk14:1.7.36")
     implementation("org.slf4j:jcl-over-slf4j:1.7.36")
@@ -94,16 +98,50 @@ dependencies {
     // This means that DiscordSRV is NOT vulnerable to CVE-2021-44228
     compileOnly("org.apache.logging.log4j:log4j-core:2.0-beta9")
 
-    // adventure API
+    // adventure, adventure-platform, MCDiscordReserializer
     val adventureVersion = "4.10.1"
-    val adventurePlatformVersion = "4.1.2"
     api("net.kyori:adventure-api:${adventureVersion}")
     api("net.kyori:adventure-text-minimessage:${adventureVersion}")
-    implementation("net.kyori:adventure-platform-bukkit:${adventurePlatformVersion}")
-    implementation("net.kyori:adventure-text-serializer-legacy:${adventureVersion}")
-    implementation("net.kyori:adventure-text-serializer-plain:${adventureVersion}")
-    implementation("net.kyori:adventure-text-serializer-gson:${adventureVersion}")
+    api("net.kyori:adventure-text-serializer-legacy:${adventureVersion}")
+    api("net.kyori:adventure-text-serializer-plain:${adventureVersion}")
+    api("net.kyori:adventure-text-serializer-gson:${adventureVersion}")
+    implementation("net.kyori:adventure-platform-bukkit:4.1.2")
+    api("dev.vankka:MCDiscordReserializer:4.2.2")
 
+    // Annotations
+    compileOnlyApi("org.jetbrains:annotations:23.0.0")
+    
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    
+    // Apache Commons, guava
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("commons-collections:commons-collections:3.2.2")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("commons-codec:commons-codec:1.15")
+    implementation("com.google.guava:guava:31.1-jre")
+    
+    // MySQL
+    compileOnly("mysql:mysql-connector-java:8.0.28") // NEWER than CraftBukkit's
+    
+    // Misc libraries
+    api("com.vdurmont:emoji-java:5.1.1")
+    implementation("org.bstats:bstats-bukkit:2.2.1")
+    implementation("com.github.zafarkhaja:java-semver:0.9.0")
+    implementation("com.github.kevinsawicki:http-request:6.0")
+    implementation("org.minidns:minidns-hla:1.0.4")
+    implementation("com.hrakaroo:glob:0.9.0")
+    implementation("org.springframework:spring-expression:5.3.16") {
+        // exclude Spring's org.apache.commons.logging adapter in favor of
+        // jcl-over-slf4j for better compatability with old log4j versions
+        exclude("spring-jcl")
+    }
+
+    ///
+    /// Plugin Hooks
+    ///
+    
     // chat hooks
     compileOnly("ru.mrbrikster:chatty-api:2.18.2")
     compileOnly("br.com.finalcraft:fancychat:1.0.2")
@@ -113,37 +151,22 @@ dependencies {
     compileOnly("com.palmergames.bukkit:TownyChat:0.45")
     compileOnly("mineverse.aust1n46:venturechat:2.20.1")
     compileOnly("com.comphenix.protocol:ProtocolLib:4.5.0")
+    
     // vanish hooks
     compileOnly("de.myzelyam:SuperVanish:6.2.0")
+    
     // permissions hooks
     compileOnly("net.luckperms:api:5.4")
+    
     // world hooks
     compileOnly("com.onarandombox.MultiverseCore:Multiverse-Core:2.4")
+    
     // misc hooks
     compileOnly("org.dynmap:dynmap-api:2.0")
-    compileOnly("ch.njol:skript:2.5")
-    compileOnly("net.milkbowl.vault:VaultAPI:1.7")
     compileOnly("com.gmail.nossr50:mcmmo:1.5.07")
+    compileOnly("net.milkbowl.vault:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.10.7")
-
-    compileOnlyApi("org.jetbrains:annotations:23.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("commons-io:commons-io:2.11.0")
-    compileOnly("commons-collections:commons-collections:3.2.2")
-    compileOnly("org.apache.commons:commons-lang3:3.12.0")
-    compileOnly("commons-codec:commons-codec:1.15")
-    compileOnly("com.google.guava:guava:31.1-jre")
-    compileOnly("mysql:mysql-connector-java:8.0.28") // NEWER than CraftBukkit's
-    api("com.vdurmont:emoji-java:5.1.1")
-    implementation("com.github.zafarkhaja:java-semver:0.9.0")
-    implementation("com.github.kevinsawicki:http-request:6.0")
-    implementation("org.minidns:minidns-hla:1.0.4")
-    implementation("com.hrakaroo:glob:0.9.0")
-    implementation("dev.vankka:MCDiscordReserializer:4.2.2")
-    implementation("org.springframework:spring-expression:5.3.16") {
-        // exclude Spring's org.apache.commons.logging adapter in favor of
-        // jcl-over-slf4j for better compatability with old log4j versions
-        exclude("spring-jcl")
-    }
+    
+    // debug hooks
+    compileOnly("ch.njol:skript:2.5")
 }
