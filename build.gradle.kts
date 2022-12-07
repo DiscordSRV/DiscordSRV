6990e671ee20a82cd472057de3e1b456c052c656
Vankka <vankka.main@gmail.com>
2022-12-07 18:22:12 +0200

    Comment on JUnit dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -241,6 +241,7 @@ dependencies {
     // debug hooks
     compileOnly("ch.njol:skript:2.5")
 
+    // JUnit
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     testImplementation("com.destroystokyo.paper:paper-api:${minecraftVersion}-R0.1-SNAPSHOT")
