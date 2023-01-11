b61c8bb0e859e48d4171ee4c014d9ecc19edb954
Austin "Scarsz" Shapiro <truescarsz@gmail.com>
2023-01-11 15:01:25 -0800

    #1490 allow voice network channels to be visible cba to do translations right now

--- src/main/java/github/scarsz/discordsrv/modules/voice/VoiceModule.java
+++ src/main/java/github/scarsz/discordsrv/modules/voice/VoiceModule.java
@@ -469,4 +469,9 @@ public class VoiceModule extends ListenerAdapter implements Listener {
     public static boolean isVoiceActivationAllowed() {
         return DiscordSRV.config().getBoolean("Network.Allow voice activation detection");
     }
+
+    public static boolean isVoiceChannelsVisible() {
+        return DiscordSRV.config().getBoolean("Network.Channels are visible");
+    }
+
 }
