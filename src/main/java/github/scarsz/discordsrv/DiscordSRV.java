cf9ef2473489170e8fccccbf335880b5d4cad4c2
Austin Shapiro <truescarsz@gmail.com>
2024-04-23 21:41:00 -0700

    Update src/main/java/github/scarsz/discordsrv/DiscordSRV.java

--- src/main/java/github/scarsz/discordsrv/DiscordSRV.java
+++ src/main/java/github/scarsz/discordsrv/DiscordSRV.java
@@ -2094,7 +2094,7 @@ public class DiscordSRV extends JavaPlugin {
             avatarUrl = !offline ? defaultUrl : offlineUrl;
             DiscordSRV.config().setRuntimeValue("AvatarUrl", avatarUrl);
 
-            DiscordSRV.error("Your AvatarUrl config option uses crafatar.com which no longer supports Discord so an alternative provider will be used.");
+            DiscordSRV.error("Your AvatarUrl config option uses crafatar.com, which no longer allows usage with Discord. An alternative provider will be used.");
             DiscordSRV.error("You should set your AvatarUrl (in config.yml) to an empty string (\"\") to get rid of this error.");
         }
 
