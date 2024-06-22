adf6cff8b7b40fa3df38dc1678a803687dcb536a
J-295 <74157600+J-295@users.noreply.github.com>
2024-06-22 16:46:18 +0100

    remove DebugLevel

--- src/main/java/github/scarsz/discordsrv/Debug.java
+++ src/main/java/github/scarsz/discordsrv/Debug.java
@@ -65,10 +65,7 @@ public enum Debug {
     }
 
     public boolean isVisible() {
-        boolean oldLevel = DiscordSRV.config().getIntElse("DebugLevel", 0) > 0;
-        if (oldLevel && matches("all")) {
-            return true;
-        }
+        if (matches("all")) return true;
 
         Set<String> debuggerCategories = DiscordSRV.getPlugin().getDebuggerCategories();
         if (!debuggerCategories.isEmpty() && debuggerCategories.stream().anyMatch(this::matches)) {
