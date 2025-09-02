DiscordSRV/DiscordSRV

commit 2d53c599613094cea97deee9171f93845df8faca

Fix alerts for commands

src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
@@ -93,7 +93,12 @@ public class AlertListener implements Listener, EventListener {
 
     public AlertListener() {
         listener = new RegisteredListener(
-                this,
+                new Listener() {
+                    @Override
+                    public String toString() {
+                        return "DiscordSRV Alerts";
+                    }
+                },
                 (listener, event) -> runAlertsForEvent(event),
                 EventPriority.MONITOR,
                 DiscordSRV.getPlugin(),
@@ -260,7 +265,7 @@ public class AlertListener implements Listener, EventListener {
     }
 
     public void unregister() {
-        HandlerList.unregisterAll(this);
+        HandlerList.unregisterAll(listener.getListener());
         registered = false;
     }
 
@@ -271,6 +276,24 @@ public class AlertListener implements Listener, EventListener {
 
     @Subscribe
     public void onDSRVEvent(github.scarsz.discordsrv.api.events.Event event) {
+        runAlertsForEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (!anyCommandTrigger) {
+            return;
+        }
+
+        runAlertsForEvent(event);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onServerCommand(ServerCommandEvent event) {
+        if (!anyCommandTrigger) {
+            return;
+        }
+
         runAlertsForEvent(event);
     }
 

src/main/java/github/scarsz/discordsrv/DiscordSRV.java
@@ -1383,6 +1383,7 @@ public class DiscordSRV extends JavaPlugin {
         alertListener = new AlertListener();
         jda.addEventListener(alertListener);
         api.subscribe(alertListener);
+        getServer().getPluginManager().registerEvents(alertListener, this);
 
         // set ready status
         if (jda.getStatus() == JDA.Status.CONNECTED) {

commit bd4ffd105006dea6cc9472075dae92c55019fbde

Fix alerts for commands being duplicated if also listening to command events directly and listening to JDA events in alerts
Regression from 2d53c59 and 21b0c01

src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
@@ -216,8 +216,16 @@ public class AlertListener implements Listener, EventListener {
                 }
 
                 try {
-                    Class<?> eventClass = Class.forName(trigger);
-                    if (!Event.class.isAssignableFrom(eventClass)) continue;
+                    Class<?> eventClass = Class.forName(getEventClassName(trigger));
+                    if (!Event.class.isAssignableFrom(eventClass)) {
+                        // Not a Bukkit event ignore (DiscordSRV & JDA events don't need to be registered explicitly)
+                        continue;
+                    }
+                    if (PlayerCommandPreprocessEvent.class.isAssignableFrom(eventClass)
+                            || ServerCommandEvent.class.isAssignableFrom(eventClass)) {
+                        // Already registered, don't register again
+                        continue;
+                    }
 
                     Method method = null;
                     Class<?> handlerListClass = eventClass;
@@ -239,7 +247,11 @@ public class AlertListener implements Listener, EventListener {
                         continue;
                     }
 
-                    if (!handlerLists.add((HandlerList) handlerList)) continue;
+                    if (!handlerLists.add((HandlerList) handlerList)) {
+                        // Ignore duplicate HandlerLists
+                        continue;
+                    }
+
                     addListener((HandlerList) handlerList);
                 } catch (ClassNotFoundException ignored) {
                     DiscordSRV.warning("Could not find event for alert trigger: " + trigger);