aa0732ed86543d01dcdf092d82ffdfb41549c237
Vankka <vankka.main@gmail.com>
2025-09-19 19:54:45 +0300

    Fix for non-command bukkit event triggers

--- src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
+++ src/main/java/github/scarsz/discordsrv/modules/alerts/AlertListener.java
@@ -396,8 +396,8 @@ public class AlertListener implements Listener, EventListener {
     }
 
     private String getEventClassName(Object event) {
-        return event.getClass().getName()
-                .replace("github.scarsz.discordsrv.dependencies.jda", "net.".concat("dv8tion.jda"));
+        String className = event instanceof String ? (String) event : event.getClass().getName();
+        return className.replace("github.scarsz.discordsrv.dependencies.jda", "net.".concat("dv8tion.jda"));
     }
 
     private String getEventName(Object event) {
